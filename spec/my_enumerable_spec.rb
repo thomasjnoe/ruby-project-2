d47a6e785e6b62e745ae3decb31b92e04dbb79fb
Thomas Noe <thomasjnoe@gmail.com>
2014-08-18 22:42:51 -0400

    Adding #my_any and #my_none

--- spec/my_enumerable_spec.rb
+++ spec/my_enumerable_spec.rb
@@ -73,4 +73,221 @@ describe "Enumerable Module" do
 		end
 	end
 
+
+	describe "#my_all?" do
+		context "with empty array" do
+			it "returns an empty array" do
+				array = [].my_all? { |x| x == 1 }
+				expect(array).to eql true
+			end
+		end
+
+		context "with an array of integers under 10" do
+			let (:array) { [1,-2,3,4,5,-6] }
+
+			it "returns true for x < 10" do
+				expect(array.my_all? { |x| x < 10 } ).to eql true
+			end
+
+			it "returns false for x > 10" do
+				expect(array.my_all? { |x| x > 10 } ).to eql false
+			end
+		end
+
+		context "with an array of strings" do
+			let (:array) { ["lol","hello","goodbye","lonely hearts club band"] }
+
+			it "returns true for array including words containing the letter 'o' " do
+				expect(array.my_all? { |x| x.include? "o" } ).to eql true
+			end
+
+			it "returns true for word lengths greater than or equal to 3" do
+				expect(array.my_all? { |x| x.length >= 3 } ).to eql true
+			end
+
+			it "returns false for word lengths less than 3" do
+				expect(array.my_all? { |x| x.length < 3 } ).to eql false
+			end
+		end
+
+		context "with a mixed array" do
+			let (:array) { ["lol","hello",4,100,{ :word => "bird", :number => 8 },"hey"] }
+
+			it "returns false for all strings" do
+				expect(array.my_all? { |x| x.class == String } ).to eql false
+			end
+
+			it "returns false for all fixnums" do
+				expect(array.my_all? { |x| x.class == Fixnum } ).to eql false
+			end
+
+			it "returns false for all hashes" do
+				expect(array.my_all? { |x| x.class == Hash } ).to eql false
+			end
+		end
+
+		context "with a string" do
+			it "raises NoMethodError" do
+				string = "Hello World!"
+				expect { string.my_all? { |x| x == "!" } }.to raise_exception NoMethodError
+			end
+		end
+
+		context "with an integer" do
+			it "raises NoMethodError" do
+				int = 5
+				expect { int.my_all? { |x| x == 5 } }.to raise_exception NoMethodError
+			end
+		end
+	end
+
+
+	describe "#my_any?" do
+		context "with empty array" do
+			it "returns an empty array" do
+				array = [].my_any? { |x| x == 1 }
+				expect(array).to eql false
+			end
+		end
+
+		context "with an array of integers under 10" do
+			let (:array) { [1,-2,3,4,5,-6] }
+
+			it "returns true for x < 10" do
+				expect(array.my_any? { |x| x < 10 } ).to eql true
+			end
+
+			it "returns false for x > 10" do
+				expect(array.my_any? { |x| x > 10 } ).to eql false
+			end
+		end
+
+		context "with an array of strings" do
+			let (:array) { ["lol","hello","goodbye","lonely hearts club band"] }
+
+			it "returns true for array including words containing 'lo' " do
+				expect(array.my_any? { |x| x.include? "lo" } ).to eql true
+			end
+
+			it "returns true for word lengths equal to 3" do
+				expect(array.my_any? { |x| x.length == 3 } ).to eql true
+			end
+
+			it "returns false for word lengths less than 3" do
+				expect(array.my_any? { |x| x.length < 3 } ).to eql false
+			end
+		end
+
+		context "with a mixed array" do
+			let (:array) { ["lol","hello",4,100,{ :word => "bird", :number => 8 },"hey"] }
+
+			it "returns true for any strings" do
+				expect(array.my_any? { |x| x.class == String } ).to eql true
+			end
+
+			it "returns true for any fixnums" do
+				expect(array.my_any? { |x| x.class == Fixnum } ).to eql true
+			end
+
+			it "returns true for any hashes" do
+				expect(array.my_any? { |x| x.class == Hash } ).to eql true
+			end
+
+			it "returns false for any floats" do
+				expect(array.my_any? { |x| x.class == Float } ).to eql false
+			end
+		end
+
+		context "with a string" do
+			it "raises NoMethodError" do
+				string = "Hello World!"
+				expect { string.my_any? { |x| x == "!" } }.to raise_exception NoMethodError
+			end
+		end
+
+		context "with an integer" do
+			it "raises NoMethodError" do
+				int = 5
+				expect { int.my_any? { |x| x == 5 } }.to raise_exception NoMethodError
+			end
+		end
+	end
+
+
+	describe "#my_none?" do
+		context "with empty array" do
+			it "returns an empty array" do
+				array = [].my_none? { |x| x == 1 }
+				expect(array).to eql true
+			end
+		end
+
+		context "with an array of integers under 10" do
+			let (:array) { [1,-2,3,4,5,-6] }
+
+			it "returns true for x < 10" do
+				expect(array.my_none? { |x| x < 10 } ).to eql false
+			end
+
+			it "returns false for x > 10" do
+				expect(array.my_none? { |x| x > 10 } ).to eql true
+			end
+		end
+
+		context "with an array of strings" do
+			let (:array) { ["lol","hello","goodbye","lonely hearts club band"] }
+
+			it "returns true for array including words containing 'lo' " do
+				expect(array.my_none? { |x| x.include? "lo" } ).to eql false
+			end
+
+			it "returns true for array including words containing 'z' " do
+				expect(array.my_none? { |x| x.include? "z" } ).to eql true
+			end
+
+			it "returns true for word lengths equal to 3" do
+				expect(array.my_none? { |x| x.length == 3 } ).to eql false
+			end
+
+			it "returns false for word lengths less than 3" do
+				expect(array.my_none? { |x| x.length == 4 } ).to eql true
+			end
+		end
+
+		context "with a mixed array" do
+			let (:array) { ["lol","hello",4,100,{ :word => "bird", :number => 8 },"hey"] }
+
+			it "returns true for any strings" do
+				expect(array.my_none? { |x| x.class == String } ).to eql false
+			end
+
+			it "returns true for any fixnums" do
+				expect(array.my_none? { |x| x.class == Fixnum } ).to eql false
+			end
+
+			it "returns true for any hashes" do
+				expect(array.my_none? { |x| x.class == Hash } ).to eql false
+			end
+
+			it "returns false for any floats" do
+				expect(array.my_none? { |x| x.class == Float } ).to eql true
+			end
+		end
+
+		context "with a string" do
+			it "raises NoMethodError" do
+				string = "Hello World!"
+				expect { string.my_none? { |x| x == "!" } }.to raise_exception NoMethodError
+			end
+		end
+
+		context "with an integer" do
+			it "raises NoMethodError" do
+				int = 5
+				expect { int.my_none? { |x| x == 5 } }.to raise_exception NoMethodError
+			end
+		end
+	end
+
+
 end
